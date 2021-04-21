1600075674645f0c97223acb7506175a6366ffc7
Karthikeyan <lkarthik76@gmail.com>
2021-04-21 12:05:52 +0530

    Update app.js

--- js/app.js
+++ js/app.js
@@ -117,7 +117,7 @@ class App{
 		// Load a glTF resource
 		loader.load(
 			// resource URL
-			`dress/chair.glb`,
+			`chair${id}.glb`,
 			// called when the resource is loaded
 			function ( gltf ) {
                 
